d141314dddd7ab51eddfe7a43594765797dd3154
pahchangarg <pahchangarg2477@gmail.com>
2022-02-03 01:14:53 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCSu0IMUhwDCpoeJjcinphylk3LNv6ogX8",
+  authDomain: "base-50fa6.firebaseapp.com",
+  projectId: "base-50fa6",
+  storageBucket: "base-50fa6.appspot.com",
+  messagingSenderId: "1073190277532",
+  appId: "1:1073190277532:web:5fda575d8a362fd191f859",
+  measurementId: "G-LLJS03MF21",
+};

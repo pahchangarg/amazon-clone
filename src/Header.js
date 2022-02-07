7dbafe38884167a38145fba301ef10aeb2908e17
pahchangarg <pahchangarg2477@gmail.com>
2022-02-08 00:02:34 +0530

    added options in nav bar

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
   return (
@@ -11,7 +12,7 @@ function Header() {
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
-        {/* Logo */}
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
@@ -28,6 +29,10 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLine"></span>
         </div>
       </div>
     </div>
